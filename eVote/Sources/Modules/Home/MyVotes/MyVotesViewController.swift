95ec7489199839817273c97105b660bdacdbf08f
Danylo Manko <danmanko5@gmail.com>
2021-04-18 00:02:36 +0300

    Fixed bug with participated votes at MyVotes

--- eVote/Sources/Modules/Home/MyVotes/MyVotesViewController.swift
+++ eVote/Sources/Modules/Home/MyVotes/MyVotesViewController.swift
@@ -39,7 +39,7 @@ final class MyVotesViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.tableView.reloadData()
+        self.viewModel.updateVotes()
     }
     
     private func setupViews() {
